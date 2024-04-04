f4029fda3422fdf618dd7eb8ae5cfa1521f13949
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2024-04-04 20:21:43 +0800

    Update UserLogin.java

--- UserLogin.java
+++ UserLogin.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class UserLogin {
 
-    public static final int MAX_TRIES = 3;
-    public static final String VALID_PASSWORD = "password";
+    public static final int maxTries = 3;
+    public static final String validPassword = "password";
 
     public static void login() throws MaximumTriesExceededException, InvalidPasswordException {
         Scanner scanner = new Scanner(System.in);
         int attempt = 0;
 
-        while (attempt < MAX_TRIES) {
+        while (attempt < maxTries) {
             System.out.println("Welcome!");
             System.out.print("Enter password: ");
             String password = scanner.nextLine();
             attempt++;
 
-            if (password.equals(VALID_PASSWORD)) {
+            if (password.equals(validPassword)) {
                 System.out.println("\nLogin successful!");
                 scanner.close();
                 return; // Exit loop on successful login
-            } else if (attempt == MAX_TRIES) {
+            } else if (attempt == maxTries) {
                 scanner.close();
                 throw new MaximumTriesExceededException("\nYou have exceeded the maximum number of login attempts.");
             } else {
